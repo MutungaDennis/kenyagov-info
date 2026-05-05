67c209f62f03437b914bb2330e4ec33399ef06d9
Dennis Mutunga <85571005+MutungaDennis@users.noreply.github.com>
2026-05-05 15:35:52 +0300

    Added the file to the correct location

---
+++ types/govuk-frontend.d.ts
@@ -0,0 +1,7 @@
+// types/govuk-frontend.d.ts
+declare module 'govuk-frontend' {
+  export function initAll(options?: unknown): void;
+  
+  // You can add more exports later if needed
+  export const version: string;
+}
