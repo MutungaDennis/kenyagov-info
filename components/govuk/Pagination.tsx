b5fa2ba989501b41bc0bb050dcc971b461bbf0a1
Dennis Mutunga <85571005+MutungaDennis@users.noreply.github.com>
2026-06-29 21:04:29 +0300

    Adjusting pagination

--- components/govuk/Pagination.tsx
+++ components/govuk/Pagination.tsx
@@ -22,25 +22,45 @@ export default function Pagination({ currentPage, totalPages, baseUrl }: Paginat
             className="govuk-link govuk-pagination__link"
             rel="prev"
           >
-            <span className="govuk-pagination__link-title">Previous</span>
+            <svg
+              className="govuk-pagination__icon govuk-pagination__icon--prev"
+              xmlns="http://www.w3.org/2000/svg"
+              height="13"
+              width="15"
+              aria-hidden="true"
+              focusable="false"
+              viewBox="0 0 15 13"
+            >
+              <path d="m6.5938-0.0078125-6.7266 6.7266 6.7441 6.4062 1.377-1.449-4.1856-3.9768h12.896v-2h-12.984l4.2931-4.293-1.414-1.414z"></path>
+            </svg>
+            <span className="govuk-pagination__link-title">
+              Previous<span className="govuk-visually-hidden"> page</span>
+            </span>
           </Link>
         )}
       </div>
 
       <ul className="govuk-pagination__list">
-        {pages.map((page) => (
-          <li key={page} className="govuk-pagination__item">
-            <Link
-              href={`${baseUrl}?page=${page}`}
-              className={`govuk-link govuk-pagination__link ${
-                page === currentPage ? 'govuk-pagination__link--current' : ''
+        {pages.map((page) => {
+          const isCurrent = page === currentPage;
+          return (
+            <li
+              key={page}
+              className={`govuk-pagination__item ${
+                isCurrent ? 'govuk-pagination__item--current' : ''
               }`}
-              aria-current={page === currentPage ? 'page' : undefined}
             >
-              {page}
-            </Link>
-          </li>
-        ))}
+              <Link
+                href={`${baseUrl}?page=${page}`}
+                className="govuk-link govuk-pagination__link"
+                aria-label={`Page ${page}`}
+                aria-current={isCurrent ? 'page' : undefined}
+              >
+                {page}
+              </Link>
+            </li>
+          );
+        })}
       </ul>
 
       <div className="govuk-pagination__next">
@@ -50,7 +70,20 @@ export default function Pagination({ currentPage, totalPages, baseUrl }: Paginat
             className="govuk-link govuk-pagination__link"
             rel="next"
           >
-            <span className="govuk-pagination__link-title">Next</span>
+            <span className="govuk-pagination__link-title">
+              Next<span className="govuk-visually-hidden"> page</span>
+            </span>
+            <svg
+              className="govuk-pagination__icon govuk-pagination__icon--next"
+              xmlns="http://www.w3.org/2000/svg"
+              height="13"
+              width="15"
+              aria-hidden="true"
+              focusable="false"
+              viewBox="0 0 15 13"
+            >
+              <path d="m8.107-0.0078125-1.4136 1.414 4.2926 4.293h-12.986v2h12.896l-4.1855 3.9766 1.377 1.4492 6.7441-6.4062-6.7246-6.7266z"></path>
+            </svg>
           </Link>
         )}
       </div>
